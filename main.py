320bb942bb54167d72914b3176af78ee831617a2
Ashutosh Singh <ashu.singh234@gmail.com>
2018-02-15 08:32:45 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session, logging
 from azure.storage.blob import BlockBlobService
 from azure.storage.blob import PublicAccess
 from azure.storage.blob import ContentSettings
@@ -21,12 +21,15 @@ block_blob_service.set_container_acl('ashu-blob-container', public_access=Public
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+  return redirect(url_for('login'))
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
+        print(username)
+        session['logged_in'] = True
+        session['username'] = username
         return redirect(url_for('dashboard'))
     return render_template('login.html')
 
@@ -37,6 +40,34 @@ def dashboard():
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
+        username = session['username']
+        try:
+           conn = mysql.connector.connect(**config)
+           print("Connection established")
+        except mysql.connector.Error as err:
+          if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with the user name or password")
+          elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+          else:
+            print(err)
+        else:
+          cursor = conn.cursor()
+          for file in request.files.getlist("file"):
+                title = request.form['title']
+                file_to_upload = file.filename
+                full_path_to_file = os.path.join(os.path.dirname(__file__), file_to_upload)
+                block_blob_service.create_blob_from_path(
+                'ashu-blob-container',
+                title,
+                full_path_to_file,
+                content_settings=ContentSettings(content_type='image/png')
+                )
+                cursor.execute("INSERT INTO images (username, title) VALUES (%s, %s);", (username, title))
+                print("Inserted",cursor.rowcount,"row(s) of data.")
+          conn.commit()
+          cursor.close()
+          conn.close()
         return render_template('complete.html')
     return render_template('upload.html')
 
@@ -44,6 +75,8 @@ def upload():
 def rating(title):
     if request.method == 'POST':
       rating = request.form['rating']
+      app.logger.info('-----------------------------')
+      app.logger.info(rating)
       return render_template('complete.html', rating=rating)
     return render_template('rating.html', title=title)
 
@@ -72,4 +105,5 @@ def viewImages():
     return render_template('viewImages.html', blobs=blobs)
 
 if __name__ == '__main__':
+    app.secret_key = 'secretkey'
     app.run(debug=True)
