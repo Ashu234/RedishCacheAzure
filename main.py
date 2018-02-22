1c7b2f8beadc3fdef30652383fab9f8cabf0392e
Ashutosh Singh <ashu.singh234@gmail.com>
2018-02-22 17:39:31 -0600

    Update main.py
    added sql connection statement

--- main.py
+++ main.py
@@ -38,8 +38,31 @@ def login():
 
 @app.route('/dashboard')
 def dashboard():
-    return render_template('dashboard.html')   
+    return render_template('dashboard.html')
+  
+@app.route('/createDB')
+def createDB():
+  fileread = open('all_month.csv','rb')
+  file_reader = csv.reader(fileread)
+  try:
+           conn = mysql.connector.connect(**config)
+           print("Connection established")
+  except mysql.connector.Error as err:
+          if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with the user name or password")
+          elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+          else:
+            print(err)
+  else:
+          cursor = conn.cursor()
+          cursor.execute("DROP TABLE IF EXISTS earthquake_table;")
+        
+          cursor.close()
+          conn.close()
+  return render_template('complete.html')
 
+  
 if __name__ == '__main__':
     app.run(debug=True)
 app.secret_key = 'secretkey'
