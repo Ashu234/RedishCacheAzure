0a5a82737dfb5044fb30572f45b0691d4f997600
Ashu234 <ashu.singh234@gmail.com>
2018-02-28 21:35:04 -0600

    modified complete.html

--- main.py
+++ main.py
@@ -82,8 +82,7 @@ def createDB():
           conn.close()
           time_end = datetime.now()
           time_diff = time_end - time_start
-          session['time_diff'] = time_diff
-  return render_template('complete.html')
+  return render_template('complete.html', time_diff=time_diff)
 
 
 if __name__ == '__main__':
